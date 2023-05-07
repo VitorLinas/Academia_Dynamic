6ab69f0708fb872f1f413a8f6e42a4843110985d
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2023-05-06 23:46:02 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -5,5 +5,8 @@ function cadastrar(){
 window.sr = ScrollReveal({reset: true});
 sr.reveal('.servicos_prestados ,.cards_planos', {
     duration: 2000,
-    rotate:{x:0, y:80, z: 0}
+    rotate:{x:0, y:80, z: 0},
+    once: true,
+    delay: 300,
+    reset: false
 });
