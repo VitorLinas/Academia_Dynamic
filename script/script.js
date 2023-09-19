61cd95526d92622236e1bbee1cf64bba917133dc
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2023-09-18 21:54:24 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,3 +10,31 @@ sr.reveal('.servicos_prestados ,.cards_planos', {
     delay: 300,
     reset: false
 });
+
+
+function calculoIMC() {
+    let peso = parseFloat(document.getElementById("peso").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let resultadoIMC = document.getElementById("resultadoIMC");
+    let meuImc =  peso / (altura * altura);
+ 
+
+    if(meuImc < 18.5){
+        
+        resultadoIMC.innerHTML = `Seu IMC é ${meuImc.toFixed(2)}: Abaixo do peso`;
+    }else if(meuImc >= 18.5 && meuImc < 24.9){
+        resultadoIMC.innerHTML = `Seu IMC é ${meuImc.toFixed(2)}: Peso saudável`;
+    }else if(meuImc >= 25 && meuImc < 29.9){
+        resultadoIMC.innerHTML = `Seu IMC é ${meuImc.toFixed(2)}: Sobrepeso`;
+    }else if(meuImc >= 30 && meuImc < 34.9){
+        resultadoIMC.innerHTML = `Seu IMC é ${meuImc.toFixed(2)}: Obesidade grau 1`;
+    }else if(meuImc >= 35 && meuImc < 39.9){
+        resultadoIMC.innerHTML = `Seu IMC é ${meuImc.toFixed(2)}: Obesidade grau 2`;
+    }else if(meuImc >= 40){
+        resultadoIMC.innerHTML = `Seu IMC é ${meuImc.toFixed(2)}: Obesidade grau 3 (obesidade mórbida)`;
+    }else{
+        window.alert("Insira um valor válido");
+    }
+
+    console.log(peso);
+}
